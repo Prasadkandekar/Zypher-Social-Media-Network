971b7bdb5b40479662a91c8622a1417d80faa883
“Prasadkandekar” <kandekarprasadtrimbak@gmail.com>
2024-12-27 10:24:26 +0530

    setup completed

---
+++ zypher/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+        <h1>Welcome to Zypher Social Media Network </h1>
+        <p>A Social Media Web app designed especially for College students</p>
+    </>
+  )
+}
+
+export default App
